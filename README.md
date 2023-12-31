7faafab00a00a1ba1807e1ed52129de2c6314f5c
vitaovich <vitaliyalekhnovich@gmail.com>
2023-12-31 12:46:57 +0000

    adjust card story args

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ A template for creating a react component library.
 
 ## Get a PAT (Personal Access Token):
 https://github.com/settings/tokens
-- use for `bash./initialize.sh`
+- use for `bash/initialize.sh`
 
 ## Reference:
 [template-react-component-library](https://github.com/alexeagleson/template-react-component-library)
--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -4,14 +4,24 @@ import Card from "./Card";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "ReactComponentLibrary/Card",
+  title: "CardComponents/Card",
   component: Card,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
+  argTypes: {
+    cardSuit: {
+      options: ['♠','☘','♦','♥'],
+      control: {type: 'select'}
+    },
+    cardValue: {
+      options: ['A','2','3','4','5','6','7','8','9','10','J','Q','K'],
+      control: {type: 'select'}
+    }
+  },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ['autodocs']
 } as Meta<typeof Card>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
